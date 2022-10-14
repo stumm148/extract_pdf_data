333e589e99cd535649ee51619831ce69dbf385cb
stumm148 <putalas@gmail.com>
2022-10-14 08:30:55 +0300

    get df series data with iloc

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import pandas as pd
 def get_data_from_pdf(file_path, pages):
     return camelot.read_pdf(file_path, pages=pages, flavor='stream')
 
+
 def filter_data(sort_list, table):
     sorted_df = table.df[0].isin(sort_list)
     return table.df.loc[sorted_df]
+
 
 def write_to_csv(file_path, data):
     df = pd.DataFrame(data)
@@ -22,7 +24,7 @@ if __name__ == "__main__":
     files_path_list = glob.glob(f"D:\python_apps\pdf_data_retrieve\*.pdf")
     pages = '2'
     paragraph_list = ["1.1.", "1.4.1.", "1.4.2."]
-    data_dict = {"file":[], "id":[], "name": [], "code": []}
+    data_dict = {"file": [], "id": [], "name": [], "code": []}
 
     start_time = time.time()
 
@@ -31,10 +33,10 @@ if __name__ == "__main__":
         tables = get_data_from_pdf(file_path=path, pages=pages)
         df_data = filter_data(sort_list=paragraph_list, table=tables[0])
 
-        data_dict['file'].append(os.path.split(path)[1].replace('.pdf', ''))
-        data_dict['id'].append(df_data[2][2])
-        data_dict['name'].append(df_data[2][12])
-        data_dict['code'].append(df_data[2][13])
+        data_dict['file'].append(os.path.split(path)[1][:-4])  # add only file name without extension (.pdf)
+        data_dict['id'].append(df_data[2].iloc[0])
+        data_dict['name'].append(df_data[2].iloc[1])
+        data_dict['code'].append(df_data[2].iloc[2])
 
     write_to_csv(file_path='data.csv', data=data_dict)
 
